b512674ba8e51e92e3c439820cd75ba16199e436
Craitson <Craitson@TOSHIBA-PC>
2016-04-14 19:50:42 -0300

    Comit Inicial

--- PongServer/src/pongserver/PongServer.java
+++ PongServer/src/pongserver/PongServer.java
@@ -5,6 +5,11 @@
  */
 package pongserver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author Craitson
@@ -14,8 +19,26 @@ public class PongServer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente "
+                + cliente.getInetAddress().getHostAddress()
+        );
+
+        Scanner s = new Scanner(cliente.getInputStream());
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());   
+        }
+
+        s.close();
+        servidor.close();
+        cliente.close();
+
     }
-    
+
 }
---
+++ PongServer/src/pongserver/tst.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pongserver;
+
+import PongOO.*;
+
+/**
+ *
+ * @author Derlei
+ */
+public class tst {
+
+    public static void main(String[] args) {
+        
+        Thread t1 = new Thread(new Jogo());
+        t1.start();
+    }
+}
